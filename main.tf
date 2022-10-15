d6dee2f522d709d5fa92e626465878cced7cf9c4
SuccessAyedun <115385537+SuccessAyedun@users.noreply.github.com>
2022-10-15 18:31:37 -0500

    Update main.tf file
    Copy output.tf file to main file to run sequence/

--- main.tf
+++ main.tf
@@ -3,3 +3,28 @@ provider "aws" {
     access_key = "127275020101"
     secret_key = "8293176014d4bc6f6a5420edf6d58cf3faa53c3dc54fd07fc18149fb637248f4"
 }-
+
+    data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
